326844ad58a4dc21dfccba6618593b6dc0654ac7
vkryvonosi001 <vitalii.kryvonosiuk@pwc.com>
2022-10-18 11:10:31 +0300

    Initial commit

---
+++ src/main/java/com/example/workbench_demo/WorkbenchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbench_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbenchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbenchDemoApplication.class, args);
+	}
+
+}
