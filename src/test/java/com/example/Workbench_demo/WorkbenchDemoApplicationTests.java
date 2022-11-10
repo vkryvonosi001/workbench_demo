652646ef5d4cb26d197cb2bd07677ec74b5ebd46
vkryvonosi001 <vitalii.kryvonosiuk@pwc.com>
2022-11-11 00:15:39 +0200

    Implement test db

--- src/test/java/com/example/Workbench_demo/WorkbenchDemoApplicationTests.java
+++ src/test/java/com/example/Workbench_demo/WorkbenchDemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.Workbench_demo;
 
+import com.example.workbench_demo.repository.TeamMemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.transaction.Transactional;
 
 @SpringBootTest
+@Transactional
+@TestPropertySource("/application-test.properties")
+@Sql(value = "/dataset/teamMemberData.sql")
 class WorkbenchDemoApplicationTests {
+	@Autowired
+	private TeamMemberRepository teamMemberRepository;
 
 	@Test
 	void contextLoads() {
+		System.out.println(teamMemberRepository.findAll().size());
 	}
 
 }
